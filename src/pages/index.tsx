221539290aea7b459271693ee2522380596237b7
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-07-16 10:49:36 -0300

    add to axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react"
 
+type Students = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  company: string;
+  skill: string;
+  grades: number;
+}
 
 export default function Home() {
+  
+  const [students, setStudents] = useState<Students[]>([])
+  
+  useEffect(() => {
+    fetch('https://api.hatchways.io/assessment/students')
+     .then(response => response.json())
+     .then(data => {
+       setStudents(data);
+    })
+  },[])
+  
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <ul>
+       {students.map(stu => {
+            return(
+                <li key={stu.firstName}>
+                 <h3>{stu.firstName}</h3>
+                </li>
+      )
+   })}
+  </ul>
   )
 }
