46ae38771c4f4bb18657f47fbc4f4f8db85ebb0a
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-08-06 22:46:14 -0300

    add a dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,7 +88,12 @@ export default function Home() {
 
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+          <input 
+          id="remember-me" 
+          name="remember-me" 
+          type="checkbox" 
+          className="h-4 w-4 text-indigo-600 
+          focus:ring-indigo-500 border-gray-300 rounded"/>
           <label className="ml-2 block text-sm text-gray-900"> Remember me </label>
         </div>
 
