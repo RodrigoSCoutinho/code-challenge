6d05ca2770ea8e8e3df082cc9af03c5faf198440
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-08-12 11:24:56 -0300

    athenticated in loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app"
 import '../styles/global.css'
+import { AuthProvider } from '../contexts/AuthContexts'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+     <AuthProvider>
+        <Component {...pageProps} />
+     </AuthProvider>
+  )
 }
 
 export default MyApp
